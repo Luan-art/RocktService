e457d030ea019ec1c47414ffce0018945d26dbf7
Luan-art <74328280+Luan-art@users.noreply.github.com>
2023-06-11 17:23:53 -0300

    versão2 Luan

--- app/src/main/java/br/edu/ifsp/dmos/view/HomeActivity.java
+++ app/src/main/java/br/edu/ifsp/dmos/view/HomeActivity.java
@@ -1,4 +1,16 @@
 package br.edu.ifsp.dmos.view;
 
-public class HomeActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ifsp.dmos.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
 }
