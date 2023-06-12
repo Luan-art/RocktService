873e0fe9e3e04301435b667025770c048c68eb8a
Luan-art <74328280+Luan-art@users.noreply.github.com>
2023-06-11 21:17:19 -0300

    Splash e alterar senha activitys

--- app/src/main/java/br/edu/ifsp/dmos/view/MenuActivity.java
+++ app/src/main/java/br/edu/ifsp/dmos/view/MenuActivity.java
@@ -1,4 +1,14 @@
 package br.edu.ifsp.dmos.view;
 
-public class MenuActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
 }
--- app/src/main/java/br/edu/ifsp/dmos/view/UpdatePasswordActivity.java
+++ app/src/main/java/br/edu/ifsp/dmos/view/UpdatePasswordActivity.java
@@ -1,4 +1,14 @@
 package br.edu.ifsp.dmos.view;
 
-public class UpdatePasswordActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdatePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_password);
+    }
 }
