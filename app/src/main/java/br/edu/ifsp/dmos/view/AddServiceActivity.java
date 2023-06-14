611b337cd7bc3e13ae89a5ca28f36a1b3d71d5ba
Luan-art <74328280+Luan-art@users.noreply.github.com>
2023-06-13 23:20:45 -0300

    Add and Edit services offeter

--- app/src/main/java/br/edu/ifsp/dmos/view/AddServiceActivity.java
+++ app/src/main/java/br/edu/ifsp/dmos/view/AddServiceActivity.java
@@ -1,4 +1,14 @@
 package br.edu.ifsp.dmos.view;
 
-public class AddServiceActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_service);
+    }
 }
--- app/src/main/java/br/edu/ifsp/dmos/view/EditServiceActivity.java
+++ app/src/main/java/br/edu/ifsp/dmos/view/EditServiceActivity.java
@@ -1,4 +1,14 @@
 package br.edu.ifsp.dmos.view;
 
-public class EditServiceActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_service);
+    }
 }
