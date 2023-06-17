8f75710d257e66d96c1eb27d5fbd414cfff38cc3
IvanFox913 <ivanfox913@hotmail.com>
2023-06-17 00:40:29 -0300

    Pequena atualização visual e mais uma toolbar

--- app/src/main/java/br/edu/ifsp/dmos/view/EditServiceActivity.java
+++ app/src/main/java/br/edu/ifsp/dmos/view/EditServiceActivity.java
@@ -1,6 +1,7 @@
 package br.edu.ifsp.dmos.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,11 @@ public class EditServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_service);
+
+        Toolbar toolbar = findViewById(R.id.edit_service_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
