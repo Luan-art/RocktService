4332f0832a677114245414ce8031a1ee2757bca4
Luan-art <74328280+Luan-art@users.noreply.github.com>
2023-06-25 17:55:05 -0300

    permissions

--- app/src/main/java/br/edu/ifsp/dmos/view/HomeActivity.java
+++ app/src/main/java/br/edu/ifsp/dmos/view/HomeActivity.java
@@ -8,6 +8,11 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -15,18 +20,22 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.SearchView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import br.edu.ifsp.dmos.R;
 import br.edu.ifsp.dmos.mvp.HomeMVP;
 import br.edu.ifsp.dmos.presenter.HomePresenter;
 
-public class HomeActivity extends AppCompatActivity implements HomeMVP.View{
+public class HomeActivity extends AppCompatActivity implements HomeMVP.View {
 
     public static final int REQUEST_PERMISSION_CODE = 999;
-    private static final String PERMISSION_GPS = Manifest.permission.ACCESS_FINE_LOCATION;
-    private static final String PERMISSION_COARSE = Manifest.permission.ACCESS_COARSE_LOCATION;
     private ImageView menuBtn;
     private SearchView search;
     private ImageButton btnTecnologia;
@@ -67,51 +76,49 @@ public class HomeActivity extends AppCompatActivity implements HomeMVP.View{
         menuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 presenter.GoToMenu(bundle);
-
             }
         });
 
         btnTecnologia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                presenter.BuscarTema("Tecnologia",bundle);
+                presenter.BuscarTema("Tecnologia", bundle);
             }
         });
 
         btnEducacao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                presenter.BuscarTema("Educação",bundle);
+                presenter.BuscarTema("Educação", bundle);
             }
         });
 
         btnCasa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                presenter.BuscarTema("Casa",bundle);
+                presenter.BuscarTema("Casa", bundle);
             }
         });
 
         btnArte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                presenter.BuscarTema("Arte",bundle);
+                presenter.BuscarTema("Arte", bundle);
             }
         });
 
         btnSaude.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                presenter.BuscarTema("Saúde",bundle);
+                presenter.BuscarTema("Saúde", bundle);
             }
         });
 
         btnEvento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                presenter.BuscarTema("Evento",bundle);
+                presenter.BuscarTema("Evento", bundle);
             }
         });
 
@@ -121,7 +128,7 @@ public class HomeActivity extends AppCompatActivity implements HomeMVP.View{
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
                     String query = search.getQuery().toString();
-                    presenter.search(query,bundle);
+                    presenter.search(query, bundle);
                     return true;
                 }
                 return false;
@@ -140,10 +147,7 @@ public class HomeActivity extends AppCompatActivity implements HomeMVP.View{
                 return false;
             }
         });
-
-
     }
-
 
     private void findById() {
         menuBtn = findViewById(R.id.menu);
@@ -155,9 +159,7 @@ public class HomeActivity extends AppCompatActivity implements HomeMVP.View{
         btnSaude = findViewById(R.id.home_btn_saude);
         btnEvento = findViewById(R.id.home_btn_evento);
         olaUser = findViewById(R.id.olaUser);
-
     }
-
 
     // Trata o resultado da solicitação de permissão
     @Override
@@ -165,39 +167,81 @@ public class HomeActivity extends AppCompatActivity implements HomeMVP.View{
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_PERMISSION_CODE) {
             for (int index = 0; index != permissions.length; index++) {
-                if (permissions[index].equalsIgnoreCase(PERMISSION_GPS) && grantResults[index] == PackageManager.PERMISSION_GRANTED) {
-                    // A permissão está concedida, você pode prosseguir com as operações relacionadas ao GPS
-                    // TODO: Adicione seu código relacionado ao GPS aqui
+                if (permissions[index].equalsIgnoreCase(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                        permissions[index].equalsIgnoreCase(Manifest.permission.ACCESS_COARSE_LOCATION)  &&
+                        grantResults[index] == PackageManager.PERMISSION_GRANTED) {
+                    localizacaoAtual();
                 } else {
-                    // A permissão foi negada, lide com isso adequadamente (por exemplo, mostre uma mensagem ao usuário)
+                    Toast.makeText(this, "Permissão a GPS negada", Toast.LENGTH_SHORT).show();
                 }
             }
         }
     }
 
     private void verificarPermissoes() {
+        String permissoes_necessarias[] = new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
 
+        List<String> requerer_permissoes = new ArrayList<>();
 
-         if (ContextCompat.checkSelfPermission(this, PERMISSION_GPS)
-                != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(this, PERMISSION_COARSE)
-                        != PackageManager.PERMISSION_GRANTED) {
-
-             ActivityCompat.requestPermissions(
-                    this,
-                    new String[]{PERMISSION_GPS, PERMISSION_COARSE},
-                    REQUEST_PERMISSION_CODE
-            );
-        } else {
-            // Todas as permissões estão concedidas, você pode prosseguir com as operações relacionadas ao GPS
-            // TODO: Adicione seu código relacionado ao GPS aqui
+        for(String permissao : permissoes_necessarias){
+            if(ContextCompat.checkSelfPermission(this, permissao) !=
+                    PackageManager.PERMISSION_GRANTED){
+                requerer_permissoes.add(permissao);
+            }
         }
-        
+        if(!requerer_permissoes.isEmpty()){
+            String pedido_permissoes[] =
+                    requerer_permissoes.toArray(
+                            new String[requerer_permissoes.size()]);
+
+            ActivityCompat.requestPermissions(
+                    this,
+                    pedido_permissoes,
+                    99);
+        } else{
+
+            localizacaoAtual();
+
+        }
+
+    }
+
+
+    private void localizacaoAtual() {
+        LocationManager mLocManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+                    if (addresses.size() > 0) {
+
+                        String cidade = addresses.get(0).getLocality();
+                        String estado = addresses.get(0).getAdminArea();
+
+
+                        bundle.putString("cidade", cidade);
+                        bundle.putString("estado", estado);
+
+                        Toast.makeText(getApplicationContext(), "Cidade: " + cidade + ", Estado: " + estado, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
     }
 
     @Override
     public Context getContext() {
         return this;
     }
-
 }
