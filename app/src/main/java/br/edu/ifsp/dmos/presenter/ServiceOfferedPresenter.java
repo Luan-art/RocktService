3bd5cc4719a607ebe9093ecf44150fe640306014
Luan-art <74328280+Luan-art@users.noreply.github.com>
2023-06-26 04:38:00 -0300

    FavoritoService

--- app/src/main/java/br/edu/ifsp/dmos/presenter/ServiceOfferedPresenter.java
+++ app/src/main/java/br/edu/ifsp/dmos/presenter/ServiceOfferedPresenter.java
@@ -35,6 +35,7 @@ public class ServiceOfferedPresenter implements ServiceOfferedMVP.Presenter {
 
     private Context context;
 
+    //construtor
     public ServiceOfferedPresenter(ServiceOfferedMVP.View serviceOfferedActivity, Context context) {
         this.view = serviceOfferedActivity;
         data = FirebaseFirestore.getInstance();
@@ -46,24 +47,31 @@ public class ServiceOfferedPresenter implements ServiceOfferedMVP.Presenter {
         view = null;
     }
 
+    // populando adapater
     @Override
     public void populate(RecyclerView recyclerView, String searchView) {
         Query query = data.collection(Constants.SERVICE_COLLECTION).orderBy(Constants.FIELD_NOME_SERVICO, Query.Direction.ASCENDING);
+        //verificar se texto de pesquisa existe ou seja diferente de nulo e maior que 0
         if (searchView != null){
             if (searchView.length() == 0){
+                //caso não seja nulo mas seja igual a zero ocorre chamada recursiva setando searchView como nulo
                 populate(recyclerView, null);
             } else{
+                //caso não seja nulo e maior que zero query receber os valores do banco de dados filtrados
                 query = data.collection(Constants.SERVICE_COLLECTION).orderBy(Constants.FIELD_NOME_SERVICO).startAt(searchView).endAt(searchView + '\uf8ff');
             }
         }
 
         FirestoreRecyclerOptions<Service> options = new FirestoreRecyclerOptions.Builder<Service>().setQuery(query, Service.class).build();
 
+        //chama adapter
         adapter = new ServiceOfferedAdapter(options, view,context);
 
+        //set o adpater dentro da recyclerView
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         recyclerView.setAdapter(adapter);
     }
+
 
     @Override
     public void startListener() {
@@ -79,19 +87,21 @@ public class ServiceOfferedPresenter implements ServiceOfferedMVP.Presenter {
 
     @Override
     public void edit(String chave) {
+        //Invoca a EditServiceActivity e envia para ela a String chave contendo o ID do service clicado pelo usuario
+
         Intent intent = new Intent(context, EditServiceActivity.class);
         intent.putExtra("chave", chave);
         context.startActivity(intent);
     }
 
     @Override
-    public void delet(String idService) {
-
-        DocumentReference documentRef = data.collection(SERVICE_COLLECTION).document(idService);
+    public void delet(String chave) {
+        //busca no banco de dados service_collection algum documento com a chave passada (id) caso encotre o deleta
+        DocumentReference documentRef = data.collection(SERVICE_COLLECTION).document(chave);
         documentRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
-
+                        //mensagem se conseguir deletar
                         Toast.makeText(context, "Serviço excluído com sucesso", Toast.LENGTH_SHORT).show();
 
                     }
@@ -99,6 +109,8 @@ public class ServiceOfferedPresenter implements ServiceOfferedMVP.Presenter {
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
+                        //mensagem se falhar ao deletar
+
                         Toast.makeText(context, "Falha ao excluir o serviço", Toast.LENGTH_SHORT).show();
 
                     }
@@ -109,6 +121,9 @@ public class ServiceOfferedPresenter implements ServiceOfferedMVP.Presenter {
 
     @Override
     public void goToAddService(Bundle bundle) {
+
+        //Invoca o AddServiceActivity
+
         Intent intent = new Intent(context, AddServiceActivity.class);
         intent.putExtras(bundle);
         context.startActivity(intent);
